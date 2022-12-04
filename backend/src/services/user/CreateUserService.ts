f6c15b4b92b4afbfb5e0574c9ba666b7328a311b
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2022-12-03 21:27:18 -0300

    Saving user

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from "../../prisma";
+
 interface UserRequest{
     name: string;
     email: string;
@@ -7,9 +9,34 @@ interface UserRequest{
   class CreateUserService{
     async execute({name, email, password}: UserRequest){
   
-      console.log(name);
+     if(!email){
+        throw new Error("Favor informar um email correto!");
+     }
+
+     const userAlreadyExists = await prismaClient.user.findFirst({
+        where:{
+            email: email
+        }
+     });
+
+     if(userAlreadyExists){
+        throw new Error("Este email já está cadastrado");
+     }
+
+     const user = await prismaClient.user.create({
+        data:{
+            name: name,
+            email: email,
+            password: password
+        },
+        select:{
+            id: true,
+            name: true,
+            email: true
+        }
+     });
   
-      return { name: name }
+      return user;
     }
   }
   
