9d3ea487d7ed5571ff8a54e11c31dc76d312e06a
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2022-12-03 22:58:53 -0300

    encrypting password

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from './controllers/user/AuthUserController'
 
 const router = Router();
 
 //-- ROTAS USER --
 router.post("/users", new CreateUserController().handle);
+//-- ROTA PARA LOGAR USUÁRIO --
+router.post("/session", new AuthUserController().handle);
 
 export { router };
