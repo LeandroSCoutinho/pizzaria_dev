207173e833d2da2d95a7157c7a07e94dc2e26d28
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2022-12-05 17:58:37 -0300

    Capturing user details

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
-import { AuthUserController } from './controllers/user/AuthUserController'
+import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailuserController } from "./controllers/user/DetailUserController";
+
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
@@ -8,5 +11,7 @@ const router = Router();
 router.post("/users", new CreateUserController().handle);
 //-- ROTA PARA LOGAR USUÁRIO --
 router.post("/session", new AuthUserController().handle);
+
+router.get("/me", isAuthenticated, new DetailuserController().handle);
 
 export { router };
