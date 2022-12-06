26dc8f72684d0705f4a163fcf8aea8845504aff9
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2022-12-05 23:06:39 -0300

    Create and list category

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,16 +2,22 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailuserController } from "./controllers/user/DetailUserController";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController"
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
 const router = Router();
 
 //-- ROTAS USER --
 router.post("/users", new CreateUserController().handle);
-//-- ROTA PARA LOGAR USUÁRIO --
+
 router.post("/session", new AuthUserController().handle);
 
 router.get("/me", isAuthenticated, new DetailuserController().handle);
+//-- ROTAS CATEGORY
+router.post('/category', isAuthenticated, new CreateCategoryController().handle );
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 export { router };
