a609bf2dde5f36b5efe22a055bc779baaddd346d
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2022-12-28 16:35:58 -0300

    Remaining in login

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 
 import { api } from '../services/apiClient';
 
@@ -52,6 +52,28 @@ export function signOut(){
 export function AuthProvider({ children }: AuthProviderProps){
   const [user, setUser] = useState<UserProps>()
   const isAuthenticated = !!user;
+
+  useEffect(() =>{
+    //Tenta pegar o valor no cookies
+    const {'@nextauth.token': token} = parseCookies();
+
+    if(token){
+      api.get('/me').then((response) =>{
+        const {id, name, email} = response.data;
+
+        setUser({
+          id,
+          name,
+          email
+        });
+      }).catch(()=>{
+        //Se der erro deslogamos o usuário.
+        signOut();
+      }); 
+       
+      
+    }
+  },[])
 
   async function signIn({ email, password }: SignInProps){
     try{
