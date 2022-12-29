ecfd2f6ba5776e7401bf04efdadb09f2e10d59a9
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2022-12-28 22:35:28 -0300

    Create header component

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,36 @@
+import styles from './style.module.scss'
+import Link from 'next/link'
+
+import {FiLogOut} from 'react-icons/fi'
+
+import { AuthContext } from '../../contexts/AuthContext'
+import {useContext} from 'react'
+
+export function Header(){
+    const {signOut} = useContext(AuthContext )
+    return(
+        <header className={styles.headerConteiner}>
+            <div className={styles.headerContent}>
+                <Link href="/dashboard" legacyBehavior>
+                    <img src="/logo.svg" width={190} height={60} />
+                </Link>
+
+                <nav className={styles.menuNav}>
+                    <Link href="/category" legacyBehavior>
+                        <a>Nova categoria</a>
+                    </Link>
+
+                    <Link href="/product" legacyBehavior>
+                        <a>Cardapio</a>
+                    </Link>
+
+                    <button onClick={signOut}>
+
+                        <FiLogOut color="#FFF" size={24} />
+                    </button>
+
+                </nav>
+            </div>
+        </header>
+    )
+}
--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,10 +1,20 @@
 import { canSSRAuth } from '../../utils/canSSRAuth'
+import { Header} from '../../components/Header'
+import Head from 'next/head'
 
 export default function Dashboard(){
   return(
-    <div>
-      <h1>Bem vindo ao painel</h1>
-    </div>
+    <>
+      <Head>
+        <title>Painel - Sujeito Pizzaria</title>
+      </Head>
+
+      <div>
+        <Header/>
+        
+        <h1>Bem vindo ao painel</h1>
+      </div>
+    </>
   )
 }
 
