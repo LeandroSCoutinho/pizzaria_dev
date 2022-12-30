5a3c29193b863ef8f87d069c0962946a65215ecb
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2022-12-29 23:12:46 -0300

    Order panel

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,19 +1,42 @@
 import { canSSRAuth } from '../../utils/canSSRAuth'
-import { Header} from '../../components/Header'
-import Head from 'next/head'
+import Head from 'next/head';
+import styles from './style.module.scss';
+
+import { Header } from '../../components/Header'
+import { FiRefreshCcw } from 'react-icons/fi'
 
 export default function Dashboard(){
   return(
     <>
-      <Head>
-        <title>Painel - Sujeito Pizzaria</title>
-      </Head>
+    <Head>
+      <title>Painel - Sujeito Pizzaria</title>
+    </Head>
+    <div>
+      <Header/>
+    
+      <main className={styles.container}>
 
-      <div>
-        <Header/>
-        
-        <h1>Bem vindo ao painel</h1>
-      </div>
+        <div className={styles.containerHeader}>
+          <h1>Últimos pedidos</h1>
+          <button>
+            <FiRefreshCcw size={25} color="#3fffa3"/>
+          </button>
+        </div>
+
+        <article className={styles.listOreders}>
+
+          <section className={styles.orderItem}> 
+            <button>
+              <div className={styles.tag}></div>
+              <span>Mesa 30</span>
+            </button>
+          </section>                  
+
+        </article>
+
+      </main>
+
+    </div>
     </>
   )
 }
